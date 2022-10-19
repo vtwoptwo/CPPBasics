bad5f76e91558d64ebe322d7e1e94c4b4531e4f9
vtwoptwo <vtwop2@gmail.com>
2022-10-19 15:59:39 +0200

    finished exercises

--- basics/S12_E3.cpp
+++ basics/S12_E3.cpp
@@ -15,35 +15,120 @@ int pn);void findAuthor(Book px[], int pn, string pauthor);
 void findYear(Book px[], int pn, int pyear);
 
 */
+#include <iostream>
 
+using namespace std; 
 
-class Book
+class book
 {
 
-private:
-    char bookname[40];
-    char bookAuthro[40];
-    int bookyear;
-    char bookpublisher[40];
-
-public: 
-    void loadBook(Book px[], int pn){
-        
-
-    }
-
-    void showBooks(Book px[], int pn) {
-
-
-    }
-
-
+public:
+    string bookName;
+    string bookAut;
+    int bookYear;
+    string bookPub;
 };
 
-int main(){
-    
-    cout << "Welcome to the Library of Vera!"; 
+void loadBook(book px[], int pn)
+{
 
+    for(int i=0; i<pn; i++)
+    {
+        cout<<"Enter the title of the book:   ";
+        cin>>px[i].bookName;
+        cout<<"Enter the author of the book:   ";
+        cin>>px[i].bookAut;
+        cout<<"Enter the year the book was published:   ";
+        cin>>px[i].bookYear;
+        cout<<"Enter the publisher of the book:   ";
+        cin>>px[i].bookPub;
 
-    return 0; 
+    }
+
 }
+
+    void showBook(book px[], int pn) {
+
+        for(int i =0; i<pn;i++){
+            cout<<"-------------------------"<<endl; 
+            cout<<px[i].bookName<<endl; 
+            cout<<px[i].bookAut<<endl; 
+            cout<<px[i].bookYear<<endl; 
+            cout<<px[i].bookPub<<endl; 
+            cout<<"-------------------------"<<endl; 
+        }
+
+
+    }
+    void findAuthor(book px[], int pn, string pauthor)
+    {
+        for(int i = 0; i < pn; i++)
+        {
+            if(pauthor == px[i].bookAut)
+            {
+
+                cout << px[i].bookName << endl;
+            };
+        };
+    }
+
+
+    void findYear(book px[], int pn, int pyear)
+    {
+          for(int i = 0; i < pn; i++)
+          {
+            if(pyear == px[i].bookYear)
+            {
+                cout << "Book titles by this author: "<< endl << px[i].bookName << endl;
+            };
+
+
+        };
+    }
+
+
+
+
+int main(){
+    int *p; 
+    int pn = 1; 
+    book books[pn]; // initialize an object of books which works as an array
+    int c;
+    string author; 
+    int year;
+    
+    // initialize a do-while loop to simulate a menu
+    do{
+        cout << "\n1. Upload a book! \n2. Search for a book by author\n3. Search for a book by year\n4. Show all books." << endl;
+        cout << "Enter 0 to exit the library." << endl; 
+        cin>>c;
+        switch (c) {
+            case 1: 
+            loadBook(books,pn);
+            break;
+
+            case 2: 
+            showBook(books,pn);
+            break;
+
+            case 3: 
+            cout<<"Which author would you like to look up? ";
+            cin>>author; 
+            findAuthor(books,pn,author);
+            break;
+
+            case 4: 
+            cin>>year;
+            findYear(books,pn, year);
+            break;
+        
+            default:
+            cout<<"Invalid choice";
+            break;
+        };
+        
+    } while(c != 0);
+   
+    cout<<"Exiting...";
+    return 0; 
+    }
